c7b1b00fff4181c4b2d5011dfa22a6ba08bc8a3f
twog93 <geraldduveau@gmail.com>
2018-03-22 11:07:34 +0100

    push

--- src/PasswordManager/Bundle/CoreBundle/Controller/HomeController.php
+++ src/PasswordManager/Bundle/CoreBundle/Controller/HomeController.php
@@ -186,11 +186,10 @@ class HomeController extends Controller
 
             $ldapbind = ldap_bind($ldapconn, $user, $rootpw) or die ("Error trying to bind: ".ldap_error($ldapconn));
             if ($ldapbind) {
-                $result = ldap_search($ldapconn, $racine,$FiltreSearch) or die ("Error in search query: " . ldap_error($ldapconn));
-                //$result = ldap_search($ldapconn, $racine,$filter) or die ("Error in search query: " . ldap_error($ldapconn));
-                dump($result);
+                //$result = ldap_search($ldapconn, $racine,$FiltreSearch) or die ("Error in search query: " . ldap_error($ldapconn));
+                $result = ldap_search($ldapconn, $racine,$filter) or die ("Error in search query: " . ldap_error($ldapconn));
+
                 $data = ldap_get_entries($ldapconn, $result);
-                dump($data);
 
             }
         }
